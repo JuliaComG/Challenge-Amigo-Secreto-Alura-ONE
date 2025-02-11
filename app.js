00146aec094d4c45f02f824273928dcbb8fd7110
Giulia <giuliaarmanelli24@gmail.com>
2025-02-11 15:10:45 -0300

    Refactor styles and improve layout for name list; update border colors and add flexbox for better item organization

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let friendsList = [];
+//let friendsList = ["Helena", "Alice", "Laura", "Maria Alice","Sophia","Manuela","Maitê","Liz","Cecília","Isabella","Luísa","Eloá","Heloísa","Júlia","Ayla","Maria Luísa","Isis","Elisa","Antonella","Valentina","Maya","Maria Júlia","Aurora","Lara","Maria Clara","Lívia","Esther","Giovanna","Sarah","Maria Cecília","Lorena","Beatriz","Rebeca","Luna","Olívia","Maria Helena","Mariana","Isadora","Melissa","Maria","Catarina","Lavínia","Alícia","Maria Eduarda","Agatha","Ana Liz","Yasmin","Emanuelly","Ana Clara","Clara","Ana Júlia","Marina","Stella","Jade","Maria Liz","Ana Laura","Maria Isis","Ana Luísa","Gabriela","Alana","Rafaela","Vitória","Isabelly","Bella","Milena","Clarice","Mirella","Ana","Emilly","Betina","Mariah","Zoe","Maria Vitória","Nicole","Laís","Melina","Bianca","Louise","Ana Beatriz","Heloíse","Malu","Melinda","Letícia","Maria Valentina","Chloe","Maria Elisa","Maria Heloísa","Maria Laura","Maria Fernanda","Ana Cecília","Hadassa","Ana Vitória","Diana","Ayla Sophia","Eduarda","Ana Lívia","Isabel","Elis","Pérola"]; //Para teste
 
 const minimumListSize = 3;
 const maximumListSize = 100;
@@ -292,4 +293,3 @@ function toggleSound() {
     soundIcon.src = isSoundEnabled ? "assets/sound-on-bl.png" : "assets/sound-off-bl.png";
     document.getElementById("name").focus();
 }
-
